6cece5f218609b21eb3bf7d3351e9f603bb23b8d
Tomás Plevak <tomiplevak@hotmail.com>
2022-06-10 18:10:58 -0300

    Pequeñas Correcciones
    Placeholder en inputs
    Nombres de variables
    Etiqueta <ul>

--- README.md
+++ README.md
@@ -1,21 +1,12 @@
 # Flame Factory Interview for Junior React Developer
 
-## Getting started
-
-```bash
-# install dependencies
-yarn install
-
-# run development server
-yarn dev
-
-# open [http://localhost:3000](http://localhost:3000).
-```
-
 ## Test
 
 [x] Create repo using this one as a template
 
-[ ] Build a page that calls /api/animals and show the list on the screen
+[x] Build a page that calls /api/animals and show the list on the screen
 
-[ ] Add a new animal calling POST /api/animals
+[x] Add a new animal calling POST /api/animals
+
+
+CHECK index.js for my answer to this test.
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,27 @@
 import { useState, useEffect } from "react";
 
 const Home = () => {
-  const [animals, setAnimals] = useState([]);
+  const [animalsList, setAnimalsList] = useState([]);
   const [name, setName] = useState([]);
   const [animal, setAnimal] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/api/animals")
       .then((response) => response.json())
-      .then((data) => setAnimals(data));
+      .then((data) => setAnimalsList(data));
   }, []);
 
   const nameChangeHandler = (event) => {
+    event.preventDefault();
     setName(event.target.value);
   };
   const animalChangeHandler = (event) => {
+    event.preventDefault();
     setAnimal(event.target.value);
   };
 
   const submitHandler = (event) => {
+    event.preventDefault();
     fetch("http://localhost:3000/api/animals", {
       headers: {
         "Content-type": "application/json",
@@ -27,32 +30,41 @@ const Home = () => {
       body: JSON.stringify({ name, animal }),
     })
       .then((response) => response.json())
-      .then((data) => setAnimals(data));
+      .then((data) => setAnimalsList(data));
   };
 
   return (
     <div>
       <h1 className="text-l font-bold">Flame Factory Animal List</h1>
-      {animals.map((animal) => (
-        <li key={animal.id}>
-          {animal.name}, {animal.animal}
-        </li>
-      ))}
+      <hr />
+      <ul>
+        {animalsList.map((animal) => (
+          <li key={animal.id}>
+            {animal.name}, {animal.animal}
+          </li>
+        ))}
+      </ul>
       <form>
         <h2 className="font-bold">New Animal</h2>
+        <hr />
         <input
           className="border border-black-200 py-3 px-4"
           id="name"
           value={name}
           onChange={nameChangeHandler}
+          placeholder="Name..."
         />
         <input
           className="border border-black-200 py-3 px-4"
           id="animal"
           value={animal}
           onChange={animalChangeHandler}
+          placeholder="Animal..."
         />
-        <button className="border bg-gray-200 py-3 px-4" onClick={submitHandler}>
+        <button
+          className="border bg-gray-200 py-3 px-4"
+          onClick={submitHandler}
+        >
           Submit
         </button>
       </form>
